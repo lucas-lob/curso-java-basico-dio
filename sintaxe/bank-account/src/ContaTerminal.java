b508b7059586112d93a04748ff3a99ef088480ec
lucas-lob <lucas.mcp23@gmail.com>
2025-04-28 12:21:53 -0300

    :sparkles: feat: finish new challenge

--- sintaxe/bank-account/src/ContaTerminal.java
+++ sintaxe/bank-account/src/ContaTerminal.java
@@ -1,5 +1,33 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String accountName;
+        String accountAgency;
+        int accountNumber;
+        float accountBalance;
+
+        Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.ENGLISH);
+
+        System.out.print("Olá! Vamos concluir o seu cadastro.\n");
+
+        System.out.print("Insira o seu nome: ");
+        accountName = scanner.nextLine();
+
+        System.out.print("\nInsira o número de sua agência: ");
+        accountAgency = scanner.nextLine();
+
+        System.out.print("\nInsira o número da sua conta: ");
+        accountNumber = scanner.nextInt();
+
+        System.out.print("\nInsira o seu saldo: ");
+        accountBalance = scanner.nextFloat();
+
+        scanner.close();
+
+        System.out.printf("\nOlá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque", accountName, accountAgency, accountNumber, accountBalance);
     }
 }
